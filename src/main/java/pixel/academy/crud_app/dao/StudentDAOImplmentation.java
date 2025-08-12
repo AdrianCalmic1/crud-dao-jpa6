30cbf24f9d9a3c5bf977f52e07a8c565c66fd240
AdrianCalmic1 <calmicadrian1@gmail.com>
2025-08-12 18:47:36 +0300

    feat: Add delete method

---
+++ src/main/java/pixel/academy/crud_app/dao/StudentDAOImplmentation.java
@@ -0,0 +1,64 @@
+package pixel.academy.crud_app.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import pixel.academy.crud_app.entity.Student;
+
+import java.util.List;
+
+@Repository
+public class StudentDAOImplmentation implements StudentDAO {
+
+    private EntityManager entityManager;
+    String offffVALEU;
+
+    @Autowired
+    public StudentDAOImplmentation(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // creating query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+        // return query results
+        return theQuery.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+
+        // preluarea studentului din baza de date
+        Student theStudent = entityManager.find(Student.class, id);
+
+        // eliminarea studentului
+        entityManager.remove(theStudent);
+
+
+
+
+    }
+
+}
